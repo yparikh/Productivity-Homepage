eae3c276038a36b1fb0c99d55f70128da718d085
yparikh <yparikh95@gmail.com>
2022-06-11 23:50:21 -0400

    Added new Theme for the header

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -2,15 +2,25 @@ import * as React from "react";
   
 // importing material UI components
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#B32428',
+    },
+  },
+});
   
 export default function Header() {
   return (
+	<ThemeProvider theme={theme}>
       <AppBar position="static">
         <Toolbar>
           {/*Inside the IconButton, we 
@@ -36,5 +46,6 @@ export default function Header() {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+	</ThemeProvider>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
+
+import "./App.css" 
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
